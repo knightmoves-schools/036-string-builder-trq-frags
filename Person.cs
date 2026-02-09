9383e0bb683846270ea00ba82345345f49196bc0
track-frags <jadirks@anamosa.k12.ia.us>
2026-02-09 18:17:56 +0000

    mmmm

--- Person.cs
+++ Person.cs
@@ -13,7 +13,7 @@ public class Person {
    string getFullName(){
       return this.firstName + " " + this.lastName;
    }
-   StringBuilder getFullNameSB(){
+   StringBuilder getFullNameBuilder(){
       StringBuilder sb = new StringBuilder();
       sb.Append(this.firstName);
       sb.Append(" ");
